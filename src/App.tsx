6780ee1fe608eaa115b4cef49b7d00290de7cfd1
Ashwin-3cS <flashashwin2004@gmail.com>
2025-11-12 09:52:54 +0530

    feat: initial commit with very basic module integration

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { ConnectButton, useCurrentAccount } from "@mysten/dapp-kit";
+import { Box, Container, Flex, Heading } from "@radix-ui/themes";
+import MintNFT from "./mintNFT";
+
+function App() {
+  const currentAccount = useCurrentAccount();
+  return (
+    <>
+      <Flex
+        position="sticky"
+        px="4"
+        py="2"
+        justify="between"
+        style={{
+          borderBottom: "1px solid var(--gray-a2)",
+        }}
+      >
+        <Box>
+          <Heading>dApp Starter Template</Heading>
+        </Box>
+
+        <Box>
+          <ConnectButton />
+        </Box>
+      </Flex>
+      <Container>
+        <Container
+          mt="5"
+          pt="2"
+          px="4"
+          style={{ background: "var(--gray-a2)", minHeight: 500 }}
+        >
+          {currentAccount ? (
+            <MintNFT />
+          ) : (
+            <Heading>Please connect your wallet</Heading>
+          )}
+        </Container>
+      </Container>
+    </>
+  );
+}
+
+export default App;
